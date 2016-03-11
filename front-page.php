f91aaa93cabce01138d6293460aee1078f7dbe22
Rumman Amin <Rumman.amin10@gmail.com>
2016-03-11 11:32:08 +0000

    Homepage Layout Edit
    Removed projects and first banner image whilst we find a better layout
    for projects.

--- front-page.php
+++ front-page.php
@@ -44,43 +44,6 @@
     </section>
   </div>
 
-  <div class="band background">
-    <section class="layout"></section>
-  </div>
-
-  <div class="band">
-    <section class="layout">
-      <h2>Our projects</h2>
-      <div class="home-projects">
-        <ul class="list-grid">
-          <?php
-            $args = array(
-            'post_type' => 'project',
-            'post__not_in' => array($post->ID),
-            'posts_per_page' => 2
-            );
-            $query = new WP_Query( $args );
-            while ($query->have_posts()) : $query->the_post();
-          ?>
-            <li>
-              <article>
-                <h3 class="post-title"><?php the_title(); ?></h3>
-                <h5><?php the_tags(' '); ?></h5>
-                <a href="<?php the_permalink(); ?>">
-                  <?php the_post_thumbnail(); ?>
-                </a>
-              </article>
-              <article>
-                <!-- <p><?php the_field('introduction'); ?></p> -->
-                <a href="<?php the_permalink(); ?>" class="button">Read more</a>
-              </article>
-            </li>
-          <?php endwhile; wp_reset_postdata(); ?>
-        </ul>
-      </div>
-    </section>
-  </div>
-
   <div class="band background background-2">
     <section class="layout"></section>
   </div>
