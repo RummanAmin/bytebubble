ed7e011aca475dd9a3fb1d8e7f156b7041a1e495
Rumman Amin <Rumman.amin10@gmail.com>
2016-05-12 19:42:43 +0100

    Post List Layout

--- front-page.php
+++ front-page.php
@@ -1,21 +1,19 @@
 <?php get_header(); ?>
 
-	<div class="band heading">
+	<!-- <div class="band heading">
     <section class="layout">
       <div class="title">
         <h1>Welcome</h1>
       </div>
     </section>
-  </div>
+  </div> -->
 
   <div class="band intro">
     <section class="layout">
       <article>
+        <h1>Welcome</h1>
         <h2><?php the_field('introduction'); ?></h2>
       </article>
-      <!-- <article>
-        <p><?php the_field('overview'); ?></p>
-      </article> -->
     </section>
   </div>
 
@@ -35,12 +33,12 @@
         <img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/linegraph.svg" alt="">
         <h3>Consultancy</h3>
         <p>If you need someone to help you manage a project, review your code or run training sessions - Give us a call.</p>
-      </article>
+      </article><!-- 
       <article>
         <img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/tools.svg" alt="">
         <h3>Branding</h3>
         <p>Your organisation is one of a kind, so make it memorable with consistent branding that sends the right message.</p>
-      </article>
+      </article> -->
     </section>
   </div>
 
@@ -50,15 +48,19 @@
 
   <div class="band extra">
     <section class="layout">
-      <h2>From the blog</h2>
+      <!-- <h2>From the blog</h2> -->
       <div>
-        <?php $the_query = new WP_Query( 'showposts=4' ); ?>
+        <?php $the_query = new WP_Query( 'showposts=3' ); ?>
         <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-          <article>
-            <h3 class="post-title"><?php the_title(); ?></h3>
-            <h5><?php the_date(); ?></h5>
-            <p><?php the_field('introduction'); ?></p>
-            <a href="<?php the_permalink(); ?>" class="button">Read more</a>
+          <article class="photo-list">
+            <a href="<?php the_permalink(); ?>">
+              <?php the_post_thumbnail('large');?> 
+              <span class="photo-list-main">         
+              <h3 class="post-title"><?php the_title(); ?></h3>
+              <h5><?php the_date(); ?></h5>
+              <p><?php the_field('introduction'); ?></p>
+              </span>  
+            </a>
           </article>
         <?php endwhile; wp_reset_postdata(); ?>
       </div>
