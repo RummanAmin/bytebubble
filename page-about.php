606b0632c0d0938ec53d783adce28003f40f8737
Rumman Amin <Rumman.amin10@gmail.com>
2016-10-02 20:05:34 +0100

    Reduced Team

--- page-about.php
+++ page-about.php
@@ -8,25 +8,15 @@
 
   <div class="band main">
     <section class="layout">
-      <article>
+      <!-- <article>
         <h3>Stuart White</h3>
         <h5>Marketing Executive</h5>
         <p>Stuart is engrossed with all things digital. Whether it is content marketing, social media, SEO or Display advertising. He has a keen eye on capitalising on marketing ventures for our clients.</p>
-      </article>
+      </article> -->
       <article>
         <h3>Rumman Amin</h3>
         <h5>Digital Project Manager</h5>
         <p>With several years of experience designing, building and managing a variety of digital projects, take our word for it - this guy knows what he's doing! He's our very own encyclopaedia of the web.</p>
-      </article>
-      <article>
-        <h3>Katie Harper</h3>
-        <h5>Designer</h5>
-        <p>Having just graduated from University, she’s just itching to put pen to paper and make her impact. Trust us on this, her work is very impressive!</p>
-      </article>
-      <article>
-        <h3>Rahima Choudhury</h3>
-        <h5>Account Manager</h5>
-        <p>Rahima has a deep understanding of the brand development process. She is the invaluable bridge between the client and our creatives.</p>
       </article>
       <article>
         <h3>Obyedur Rahman</h3>
@@ -34,20 +24,30 @@
         <p>A chartered accountant with years of experience in the corporate environment, Obi is our first port of call for all new projects and long-term partnerships.</p>
       </article>
       <article>
+        <h3>Katie Harper</h3>
+        <h5>Designer</h5>
+        <p>Having just graduated from University, she’s just itching to put pen to paper and make her impact. Trust us on this, her work is very impressive!</p>
+      </article>
+      <!-- <article>
+        <h3>Rahima Choudhury</h3>
+        <h5>Account Manager</h5>
+        <p>Rahima has a deep understanding of the brand development process. She is the invaluable bridge between the client and our creatives.</p>
+      </article> -->
+      <article>
         <h3>Kwame Wallace</h3>
-        <h5>Senior Developer</h5>
+        <h5>Web Developer</h5>
         <p>Kwame has many years experiences as a developer and loves to try out the latest technologies – which makes him ideally placed to decide precisely what technology we use for our clients’ projects.</p>
       </article>
       <article>
         <h3>Faz Ahmed</h3>
-        <h5>Senior Developer</h5>
+        <h5>Web Developer</h5>
         <p>Faz has worked in IT for over 15 years, been programming for over 20 years and has worked for some of the world's largest IT providers. He’s also got a substantial collection of very old books.</p>
       </article>
-      <article>
-        <h3>Tasnim Shofique</h3>
+      <!-- <article>
+        <h3>Katie Harper</h3>
         <h5>Designer</h5>
-        <p>Tasnim left the world of Law to pursue her love of pixels. She quickly developed a passion for design which has never left her and now acts as our master of all things Adobe.</p>
-      </article>
+        <p>Sophia left the world of Law to pursue her love of pixels. She quickly developed a passion for design which has never left her and now acts as our master of all things Adobe.</p>
+      </article> -->
       <article>
         <h3>Hayley Williams</h3>
         <h5>Front-end Developer</h5>
